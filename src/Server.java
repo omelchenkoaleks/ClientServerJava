6df88b1f630dc39a11ae40802ccbcdcdeaa00fd1
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-12-04 17:24:58 +0200

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,13 @@
+//import java.net.*;
+//import java.io.*;
+//
+//public class Server {
+//
+//    public static void main(String[] args) {
+//
+//        // Этот сокет используется для того, чтобы создать серверное подключение
+//        // и на сервере создать ожидание клиента.
+//        ServerSocket serverSocket
+//
+//    }
+//}
