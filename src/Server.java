b6ed8ecb351fac52884d649ce9e0c333652ab06f
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-12-05 07:33:59 +0200

    Добавил бесконечный цикл, чтобы сервер был не одноразовый.

--- src/Server.java
+++ src/Server.java
@@ -7,20 +7,18 @@ public class Server {
 
         ServerSocket serverSocket = new ServerSocket(8000);
 
-        // Создаем связь с клиентом через сокет:
-        Socket clientSocket = serverSocket.accept();
+        while (true) {
 
-        // Сделаем обертку над OutputStream, чтобы была возможность отправлять строки
-        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-        writer.write("<h1>Java</h1");
-        // Гарантирует, что сообщение будет прямо сейчас отправлено
-        writer.flush();
+            Socket clientSocket = serverSocket.accept();
 
-        // Как что-нибудь отправить через этот сокет?
-//        clientSocket.getOutputStream().write(64);
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("<h1>Java</h1");
+            writer.flush();
 
-        writer.close();
-        clientSocket.close();
-        serverSocket.close();
+            writer.close();
+            clientSocket.close();
+
+        }
+//        serverSocket.close();
     }
 }
